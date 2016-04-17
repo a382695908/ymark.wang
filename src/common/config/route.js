fcccbea13816ff837df2e922a98fb7857e44e365
王小伟 <wangxiaowei@wangxuelideMacBook-Pro.local>
2016-04-17 22:13:18 +0800

    add mcourse,editor,jsmind

--- src/common/config/route.js
+++ src/common/config/route.js
@@ -6,17 +6,17 @@ export default [
 	// /manage/article -> manage/article/index
 	// 在article.js中 定义indexAction(){ return this.display('list'); }
 	// 这样就实现了自动输出模板
-	[/^manage\/(\w+)$/ ,{
-		get 	: "manage/:1/index" ,
-		post 	: "manage/:1/save"
+	[/^mcourse\/(\w+)$/ ,{
+		get 	: "mcourse/:1/index" ,
+		post 	: "mcourse/:1/save"
 	}],
 
 
 
-	[/^manage\/(\w+)\/(\w+)$/, {
-	    get 	: "manage/:1/detail?id=:2",
-	    delete 	: "manage/:1/delete?id=:2",
-	    put 	: "manage/:1/save?id=:2"
+	[/^mcourse\/(\w+)\/(\w+)$/, {
+	    get 	: "mcourse/:1/detail?id=:2",
+	    delete 	: "mcourse/:1/delete?id=:2",
+	    put 	: "mcourse/:1/save?id=:2"
 	}],
 	["manage/article/:year/:month", "manage/article/list"]
 ];
