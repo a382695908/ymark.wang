04046a71ce04a1add3f613f510dd3b837c23d7de
ymma <328179934@qq.com>
2016-04-21 11:55:53 +0800

    fix

--- src/mcourse/controller/base.js
+++ src/mcourse/controller/base.js
@@ -4,4 +4,11 @@ export default class extends think.controller.base {
   /**
    * some base method in here
    */
+  __before(){
+  	let http = this.http;
+  	// console.log(http.version);
+  	// console.log(http.headers);
+  	// console.log(http.host);
+  	//判断COOKIE中是否有信息
+  }
 }
--- src/mcourse/controller/course.js
+++ src/mcourse/controller/course.js
@@ -1,5 +1,6 @@
 'use strict';
-
+let UID = require('node-uuid');
+let DateFormat = require('dateformat');
 import Base from './base.js';
 
 export default class extends Base {
@@ -13,7 +14,6 @@ export default class extends Base {
     }
 
     async deleteAction(){
-        console.log();
         let id = this.get().id;
         if(!id) return this.fail({errno: -1, errmsg: '请填写完整参数'});
         let model_tree = this.model('tree');
@@ -23,25 +23,25 @@ export default class extends Base {
 
     async saveAction(){
         let param = this.post();
-
         let uid = param.uid ;
+        let time = DateFormat(new Date(), "yyyy-mm-dd hh:MM:ss");
         let row = {
             name        : param.name , //课程名称
-            // coverpics   : param.coverpics , //封面（存图片的，多张图片用逗号隔开）
+            coverpics   : param.coverpics , //封面（存图片的，多张图片用逗号隔开）
             summary     : param.summary , //简要介绍
-            // reurl       : param.reurl , //关联的资料的路径（一个课程可以绑定一本所参考的教材，这里存的是教材的路径）
-            lasttime    : new Date() , //最后更新时间
+            reurl       : param.reurl , //关联的资料的路径（一个课程可以绑定一本所参考的教材，这里存的是教材的路径）
+            lasttime    : time , //最后更新时间
         }
         let model = this.model('course');
         if(!uid){
-             let insertId = await model.add(row);
-             console.log(insertId);
-             return this.success('添加成功');
-            // row.
-        }else{
+            uid = UID.v1();
+            row.uid = uid;
             row.adduser = '1' ; //创建人
-            row.addtime = new Date(); //创建时间
+            row.addtime = time; //创建时间
             row.status  = 0 ; //状态（0：未上线，1：已上线，2：已下线）
+            await model.add(row);
+            return this.success(uid);
+        }else{
             let insertId = await model.update(row).where({uid:uid});
             console.log(insertId);
             return this.success('修改成功');
