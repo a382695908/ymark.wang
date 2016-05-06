1ce144e0c68004bcb4a4bbf2f358f3e1c74838a8
ymma <328179934@qq.com>
2016-05-06 19:05:22 +0800

    测试缓存

--- src/course/controller/base.js
+++ src/course/controller/base.js
@@ -1,9 +1,21 @@
 'use strict';
 
 export default class extends think.controller.base {
-  	__before(){
-	  	this.getUserId = function(){
-	        return '1';
-	    }
+	__before() {
+		const allowAccessOrigin = this.http.headers.origin;
+		// console.log(this.http.res);
+		// this.header('Access-Control-Allow-Origin', allowAccessOrigin);
+		// this.header('Access-Control-Allow-Headers', 'Content-Type,Content-Length, Authorization, Accept,X-Requested-With');
+		// this.header('Access-Control-Allow-Credentials', 'true');
+		this.header("Cache-Control", "no-cache, no-store, must-revalidate");
+		this.header("Pragma", "no-cache");
+		this.header("Expires", 0);
+
+		// res.header("Cache-Control", "no-cache, no-store, must-revalidate");
+		//   	res.header("Pragma", "no-cache");
+		//   	res.header("Expires", 0);
+		this.getUserId = function() {
+			return '1';
+		}
 	}
 }
